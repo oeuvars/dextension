65c3e9424d1ce726fe280ac47098f6ddf3e56b53
Anurag Das <oeuvars@gmail.com>
2025-03-31 01:42:39 +0530

    fix: footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,20 +11,20 @@ const space = Space_Grotesk({
 });
 
 export const metadata: Metadata = {
-  title: "dextension | VS Code Extension Downloader",
-  description: "dextension | Generate download links for VS Code extensions to install in your favorite editor",
+  title: "Dextension | VS Code Extension Downloader",
+  description: "Dextension | Generate download links for VS Code extensions to install in your favorite editor",
   keywords: ["VS Code", "extensions", "cursor", "windsurf", "Trae", "download", "VSIX"],
   authors: [{ name: "Anurag Das" }],
   creator: "Anurag | Oeuvars",
   openGraph: {
-    title: "VS Code Extension Downloader for Trae",
-    description: "dextension | Generate download links for VS Code extensions to install in your favorite editor",
+    title: "Dextension",
+    description: "Dextension | Generate download links for VS Code extensions to install in your favorite editor",
     type: "website",
     locale: "en_US",
   },
   twitter: {
     card: "summary_large_image",
-    title: "VS Code Extension Downloader for Trae",
+    title: "Dextension | VS Code Extension Downloader",
     description: "dextension | Generate download links for VS Code extensions to install in your favorite editor",
   },
   icons: {
